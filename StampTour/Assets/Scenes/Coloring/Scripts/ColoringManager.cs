68baf59d1b014560c88788e6db22beb90aa2ec1c
Meongjeong <258xsw@naver.com>
2024-05-01 11:07:08 +0900

    [feat] Show Current color

--- StampTour/Assets/Scenes/Coloring/Scripts/ColoringManager.cs
+++ StampTour/Assets/Scenes/Coloring/Scripts/ColoringManager.cs
@@ -8,14 +8,18 @@ public class ColoringManager : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private Color selectedColor;
+    public Image currentColor;
     void Start()
     {
         selectedColor = Color.white;
+        currentColor.color = Color.white;
     }
     public void SelectColor()
     {
         GameObject thisBtn = EventSystem.current.currentSelectedGameObject;
         selectedColor = thisBtn.GetComponent<Image>().color;
+        currentColor.color = selectedColor;
+
     }
     public void ChangeColor()
     {
