144af640b94ac96c79c269920a619bf58d02c31b
Meongjeong <258xsw@naver.com>
2024-05-23 09:40:16 +0900

    [feat] Add game clear check to coloring scene & sound effects to coloring
    - 색칠할 때 색칠하는 사운드 추가
    - 캐릭터 3개 다 칠한 경우에만 Main Scene으로 돌아가는 기능(ColoringScenario.GameClear) 추가

--- StampTour/Assets/Scenes/Coloring/Scripts/ColoringManager.cs
+++ StampTour/Assets/Scenes/Coloring/Scripts/ColoringManager.cs
@@ -10,24 +10,33 @@ public class ColoringManager : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] private Color selectedColor;
     public Image currentColor;
+    public AudioClip coloringAudio;
+    public AudioClip pickAudio;
+    AudioSource audioSource;
 
    
     void Start()
     {
         selectedColor = Color.white;
         currentColor.color = Color.white;
+        audioSource = GetComponent<AudioSource>();
+
     }
     public void SelectColor()
     {
         GameObject thisBtn = EventSystem.current.currentSelectedGameObject;
         selectedColor = thisBtn.GetComponent<Image>().color;
         currentColor.color = selectedColor;
+        audioSource.clip = pickAudio;
+        audioSource.Play();
 
     }
     public void ChangeColor()
     {
         GameObject thisBtn = EventSystem.current.currentSelectedGameObject;
         thisBtn.GetComponent<Image>().color = selectedColor;
+        audioSource.clip = coloringAudio;
+        audioSource.Play();
     }
     
 
