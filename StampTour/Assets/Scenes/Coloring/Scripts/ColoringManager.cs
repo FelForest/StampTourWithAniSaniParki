18f65a8183c4d73c163e423815102575b176d72b
Meongjeong <258xsw@naver.com>
2024-05-27 16:36:15 +0900

    [chore] Material war

--- StampTour/Assets/Scenes/Coloring/Scripts/ColoringManager.cs
+++ StampTour/Assets/Scenes/Coloring/Scripts/ColoringManager.cs
@@ -10,37 +10,24 @@ public class ColoringManager : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] private Color selectedColor;
     public Image currentColor;
-    public AudioClip coloringAudio;
-    public AudioClip pickAudio;
-    public GameObject test1;
-    public GameObject test2;
-    AudioSource audioSource;
 
    
     void Start()
     {
         selectedColor = Color.white;
         currentColor.color = Color.white;
-        audioSource = GetComponent<AudioSource>();
-
     }
     public void SelectColor()
     {
         GameObject thisBtn = EventSystem.current.currentSelectedGameObject;
         selectedColor = thisBtn.GetComponent<Image>().color;
         currentColor.color = selectedColor;
-        audioSource.clip = pickAudio;
-        audioSource.Play();
 
     }
     public void ChangeColor()
     {
         GameObject thisBtn = EventSystem.current.currentSelectedGameObject;
-        if(thisBtn.name == "test1") test1.GetComponent<Material>().color = selectedColor;
-        if(thisBtn.name=="test2") test2.GetComponent<Material>().color = selectedColor;
         thisBtn.GetComponent<Image>().color = selectedColor;
-        audioSource.clip = coloringAudio;
-        audioSource.Play();
     }
     
 
